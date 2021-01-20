dc19c5afcb2270e7456f9096dcf2dcb73364312f
Liteskarr <398501z3@gmail.com>
2021-01-20 20:08:58 +0300

    connecting unit_type with pow_modifiers

---
+++ src/unit_type.py
@@ -0,0 +1,43 @@
+"""
+TODO: Написать то, зачем это нужно.
+"""
+
+from src.pow_modifier import POWModifier
+from src.unit_group_container import UnitGroupsContainer
+
+
+class UnitType:
+    @staticmethod
+    def get_groups() -> UnitGroupsContainer:
+        """
+        Возвращает группы, к которым относится тип юнита.
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def get_default_pow() -> int:
+        """
+        Возвращает дефолтное количество очков POW для данного типа юнитов.
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def get_default_moving_points() -> int:
+        """
+        Возвращает дефолтное количество очков движения для данного типа юитов.
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def on_attacking_against(groups: UnitGroupsContainer) -> POWModifier:
+        """
+        Возвращает модификатор к POW для данного юнита в атаке против юнита, имеющего данные группы.
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def on_defending_against(groups: UnitGroupsContainer) -> POWModifier:
+        """
+        Возвращает модификатор к POW для данного юнита в защите против юнита, имеющего данные группы.
+        """
+        raise NotImplementedError()
