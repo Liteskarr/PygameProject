812333e060cc577064e8400c1cf6831b9737a621
Liteskarr <398501z3@gmail.com>
2021-01-27 18:08:27 +0300

    docs

--- src/terrain.py
+++ src/terrain.py
@@ -2,24 +2,51 @@
 Интерфейс, определяющий поведение типа местности.
 """
 
+from typing import Union
+
 from src.pow_modifier import POWModifier, NonePOWModifier
+from src.terrain_resource import TerrainResource, NoneTerrainResource
 
 
 class Terrain:
+    """
+    Интерфейс, описывающий поведение местности на игровой карте.
+    """
+
     @staticmethod
     def get_defencive_bonus() -> POWModifier:
+        """
+        Возвращает модификатор к защите юнита на карте.
+        """
         raise NotImplementedError()
 
     @staticmethod
     def get_attacking_bonus() -> POWModifier:
+        """
+        Возвращает модификатор к атаке юнита на карте.
+        """
         raise NotImplementedError()
 
     @staticmethod
     def get_movement_cost() -> int:
+        """
+        Возвращает стоимость пересечения данной местности.
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def get_resource() -> Union[TerrainResource, type]:
+        """
+        Возвращает ресурсы для визуализации типа местности на карте.
+        """
         raise NotImplementedError()
 
 
 class NoneTerrain(Terrain):
+    """
+    Реализация None-паттерна для класса Terrain.
+    """
+
     @staticmethod
     def get_defencive_bonus() -> POWModifier:
         return NonePOWModifier()
@@ -31,3 +58,7 @@ class NoneTerrain(Terrain):
     @staticmethod
     def get_movement_cost() -> int:
         return 1
+
+    @staticmethod
+    def get_resource() -> Union[TerrainResource, type]:
+        return NoneTerrainResource
