7b14b34132d33ce85a8128efcb26fde8b80ea198
Liteskarr <398501z3@gmail.com>
2021-01-27 17:58:49 +0300

    docs adding reference to biome resource

--- src/biome.py
+++ src/biome.py
@@ -1,17 +1,37 @@
-"""
-Интерфейс, определяющий поведение биома.
-"""
+from typing import Union
 
-from src.pow_modifier import POWModifier
+from src.biome_resource import BiomeResource, NoneBiomeResource
 
 
 class Biome:
+    """
+    Интерфейс, определяющий поведение биома.
+    """
+
     @staticmethod
     def get_supply_cost() -> int:
+        """
+        Возвращает цену снабжения при ее распространении в данном биоме.
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def get_resource() -> Union[BiomeResource, type]:
+        """
+        Возвращает класс, содержащий ресурсы, необходимые для визуализации биома.
+        """
         raise NotImplementedError()
 
 
 class NoneBiome(Biome):
+    """
+    Реализация None-паттерна для интерфейса Biome.
+    """
+
     @staticmethod
     def get_supply_cost() -> int:
         return 1
+
+    @staticmethod
+    def get_resource() -> Union[BiomeResource, type]:
+        return NoneBiomeResource
