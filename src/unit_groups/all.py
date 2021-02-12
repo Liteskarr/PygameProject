e50237263060f765006251e1760bd4be66fc18fc
Liteskarr <398501z3@gmail.com>
2021-02-13 02:46:43 +0300

    feature

---
+++ src/unit_groups/all.py
@@ -0,0 +1,6 @@
+from src.unit_groups.artillery import *
+from src.unit_groups.cavalry import *
+from src.unit_groups.heavy import *
+from src.unit_groups.infantry import *
+from src.unit_groups.light import *
+from src.unit_groups.support import *
